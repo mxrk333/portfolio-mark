b95a75a909f2e0158300ca376839b1b9c77ccb7f
mxrk333 <markdeath455@gmail.com>
2025-07-02 01:42:05 +0800

    adding scripts

---
+++ assets/script/next.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from 'next'
+import { withContentlayer } from 'next-contentlayer'
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+}
+
+export default nextConfig
